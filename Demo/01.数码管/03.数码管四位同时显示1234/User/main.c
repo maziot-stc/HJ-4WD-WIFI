2d687bdbf72f317ec99fa1085245357d79b00680
mz8023yt <mz8023yt@163.com>
2019-05-04 21:07:02 +0800

    [FEAT] 实现4位数码管同时显示1234

--- Demo/01.数码管/03.数码管四位同时显示1234/User/main.c
+++ Demo/01.数码管/03.数码管四位同时显示1234/User/main.c
@@ -9,20 +9,20 @@
 u8 SegCode[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};      // 段选低电平有效
 u8 DigCode[4] = {0xfe, 0xfd, 0xfb, 0xf7};                                           // 位选低电平有效
 
+/* 数码管显示的值 */
+int value[4] = {1, 2, 3, 4};
+
 int main(void)
 {
     int i = 0;
 
     while(1)
     {
-        Digital = DigCode[0];
-        Segment = SegCode[i];
-        delay_ms(1000);
-
-        i++;
-        if(i > 9)
+        for(i = 0; i < 4; i++)
         {
-            i = 0;
+            Digital = DigCode[i];
+            Segment = SegCode[value[i]];
+            delay_ms(1);
         }
     }
 }
