f7d3766a0aa5508f48401e8ea57390ad8b0fa020
mz8023yt <mz8023yt@163.com>
2019-05-04 21:26:37 +0800

    [FEAT] 实现数码管流水灯显示

--- Demo/01.数码管/05.数码管显示流水灯/User/main.c
+++ Demo/01.数码管/05.数码管显示流水灯/User/main.c
@@ -9,8 +9,13 @@
 u8 SegCode[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};      // 段选低电平有效
 u8 DigCode[4] = {0xfe, 0xfd, 0xfb, 0xf7};                                           // 位选低电平有效
 
-/* 数码管显示的值 */
-int value[4] = {1, 2, 3, 4};
+/* 数码管流水灯段选码、位选码 */
+u8 LightSegCode[12] = {0xfe, 0xfe, 0xfe, 0xfe, \
+                       0xfd, 0xfb, 0xf7, 0xf7, \
+                       0xf7, 0xf7, 0xef, 0xdf};
+u8 LightDigCode[12] = {0xfe, 0xfd, 0xfb, 0xf7,
+                       0xf7, 0xf7, 0xf7, 0xfb, \
+                       0xfd, 0xfe, 0xfe, 0xfe};
 
 int main(void)
 {
@@ -18,11 +23,11 @@ int main(void)
 
     while(1)
     {
-        for(i = 0; i < 4; i++)
+        for(i = 0; i < 12; i++)
         {
-            Digital = DigCode[i];
-            Segment = SegCode[value[i]];
-            delay_ms(1);
+            Digital = LightDigCode[i];
+            Segment = LightSegCode[i];
+            delay_ms(100);
         }
     }
 }
