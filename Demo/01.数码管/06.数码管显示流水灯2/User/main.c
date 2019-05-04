a1c6ed23f428018fd539156ef6c60949a15c8604
mz8023yt <mz8023yt@163.com>
2019-05-04 22:25:21 +0800

    [FEAT] 实现数码管流水灯2

--- Demo/01.数码管/06.数码管显示流水灯2/User/main.c
+++ Demo/01.数码管/06.数码管显示流水灯2/User/main.c
@@ -17,17 +17,47 @@ u8 LightDigCode[12] = {0xfe, 0xfd, 0xfb, 0xf7,
                        0xf7, 0xf7, 0xf7, 0xfb, \
                        0xfd, 0xfe, 0xfe, 0xfe};
 
+int value[4] = {0xff, 0xff, 0xff, 0xff};
+
 int main(void)
 {
     int i = 0;
+    int count = 0;                      /* 数码管动态扫描次数计数 */
+    int index = 0;                      /* 流水灯分为12个小灯, 流水灯计数 */
 
     while(1)
     {
-        for(i = 0; i < 12; i++)
+        for(i = 0; i < 4; i++)
         {
-            Digital = LightDigCode[i];
-            Segment = LightSegCode[i];
-            delay_ms(100);
+            /* 数码管动态显示 */
+            Digital = DigCode[i];
+            Segment = value[i];
+            delay_ms(1);
+        }
+        count++;
+
+        /* 修改数码管显示的值 */
+        if(count > 50)
+        {
+            if(index >= 12)
+            {
+                for(i = 0; i < 4; i++)
+                {
+                    value[i] = 0xff;
+                }
+                index = 0;
+            }
+
+            /* 判断当前流水灯是哪个位选上的，将该位需要点亮的位置写入value数组中 */
+            for(i = 0; i < 4; i++)
+            {
+                if(LightDigCode[index] == DigCode[i])
+                {
+                    value[i] = value[i] & LightSegCode[index];
+                }
+            }
+            index++;
+            count = 0;
         }
     }
 }
