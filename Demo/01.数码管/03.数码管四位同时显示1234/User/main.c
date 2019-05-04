476d28597b60956757278f9394c4340993b9b743
mz8023yt <mz8023yt@163.com>
2019-05-04 20:56:41 +0800

    [CLONE] 基于01.02开发01.03

---
+++ Demo/01.数码管/03.数码管四位同时显示1234/User/main.c
@@ -0,0 +1,28 @@
+#include "reg52.h"
+#include "main.h"
+#include "bsp_delay.h"
+
+#define Segment P0
+#define Digital P2
+
+/* 数码管段选码、位选码 */
+u8 SegCode[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};      // 段选低电平有效
+u8 DigCode[4] = {0xfe, 0xfd, 0xfb, 0xf7};                                           // 位选低电平有效
+
+int main(void)
+{
+    int i = 0;
+
+    while(1)
+    {
+        Digital = DigCode[0];
+        Segment = SegCode[i];
+        delay_ms(1000);
+
+        i++;
+        if(i > 9)
+        {
+            i = 0;
+        }
+    }
+}
