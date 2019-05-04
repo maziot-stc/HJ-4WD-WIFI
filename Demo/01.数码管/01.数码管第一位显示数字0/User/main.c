22c7844806569f0552e7b3fcdf62996e8d940321
mz8023yt <mz8023yt@163.com>
2019-05-04 20:23:04 +0800

    [FEAT] 实现第一位数码管显示数字0

---
+++ Demo/01.数码管/01.数码管第一位显示数字0/User/main.c
@@ -0,0 +1,17 @@
+#include "reg52.h"
+#include "main.h"
+
+#define Segment P0
+#define Digital P2
+
+/* 数码管段选码、位选码 */
+u8 SegCode[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x91, 0x81, 0xf8, 0x80, 0x90};      // 段选低电平有效
+u8 DigCode[4] = {0xfe, 0xfd, 0xfb, 0xf7};                                           // 位选低电平有效
+
+int main(void)
+{
+    Digital = DigCode[0];
+    Segment = SegCode[0];
+
+    while(1);
+}
