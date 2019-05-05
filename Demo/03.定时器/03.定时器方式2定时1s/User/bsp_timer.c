894aee3490de4bfacc51d4b3f9e792a5e919c203
mz8023yt <mz8023yt@163.com>
2019-05-05 23:38:34 +0800

    [CLONE] 基于03.01开发03.03

---
+++ Demo/03.定时器/03.定时器方式2定时1s/User/bsp_timer.c
@@ -0,0 +1,66 @@
+#include "bsp_timer.h"
+#include "reg52.h"
+
+static interrupt_count = 0;
+extern void run(void);
+
+/**
+ * @Description 定时器0简单初始化函数
+ */
+void timer_init(void)
+{
+    /* 定时器0工作在方式0: 13位定时计数模式 */
+    /* 定时器13位组成，TL0低5位，TH08位，其中TL0低5位溢出向TH0进位 */
+    TMOD = 0x00;
+
+    /* 方式0: 13位定时计数器，最大计数值 2^13 = 8192 */
+    /* HJ-4WD-WIFI 外挂晶振频率为 11.0592Mhz <==> 11059200Hz */
+    /* 定时器使用系统时钟的12分频作为计数频率，即 11059200/12 = 921600Hz*/
+    /* 定时器需要计数 921600 个数才能得到 1s 时间 */
+    /* 将 921600 分解为 7680*120，即定时器每次计数 7680 个数，计数 120 次 */
+    /* 因此定时器计数初值为 2^13-7680 = 512 = 0x200 */
+    /* 将 0x200 填入 TH0[7:0] 和 TL0[4:0] 中 */
+    TH0 = 0x10;
+    TL0 = 0x00;
+
+    /* 使能总中断和定时器0中断 */
+    IE = 0x82;
+}
+
+/**
+ * @Description 使能定时器0
+ */
+void timer_start(void)
+{
+    TR0 = 1;
+}
+
+/**
+ * @Description 关闭定时器0
+ */
+void timer_stop(void)
+{
+    TR0 = 0;
+}
+
+/**
+ * @Descroption 定时器0中断服务函数
+ */
+void timer0_handler() interrupt 1
+{
+    timer_stop();
+
+    /* 手动的装载初值 */
+    TH0 = 0x10;
+    TL0 = 0x00;
+
+    timer_start();
+
+    interrupt_count++;
+
+    if(interrupt_count >= 120)
+    {
+        interrupt_count = 0;
+        run();
+    }
+}
---
+++ Demo/03.定时器/03.定时器方式2定时1s/User/main.c
@@ -0,0 +1,74 @@
+#include "reg52.h"
+#include "main.h"
+#include "bsp_delay.h"
+#include "bsp_timer.h"
+
+#define Segment P0
+#define Digital P2
+
+/* 数码管段选码、位选码 */
+u8 SegCode[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};      // 段选低电平有效
+u8 DigCode[4] = {0xfe, 0xfd, 0xfb, 0xf7};                                           // 位选低电平有效
+
+/* 数码管显示的值 */
+int value[4] = {0, 0, 0, 0};
+
+int main(void)
+{
+    int i = 0;
+
+    timer_init();
+    timer_start();
+
+    while(1)
+    {
+        for(i = 0; i < 4; i++)
+        {
+            Digital = DigCode[i];
+            Segment = (1 == i) ? (SegCode[value[i]] & 0x7f) : SegCode[value[i]];
+            delay_ms(1);
+        }
+    }
+}
+
+/**
+ * @Descroption 定时器0中断回调函数
+ */
+void run(void)
+{
+    if(value[3] < 9)
+    {
+        value[3]++;
+    }
+    else
+    {
+        value[3] = 0;
+        if(value[2] < 5)
+        {
+            value[2]++;
+        }
+        else
+        {
+            value[2] = 0;
+            if(value[1] < 9)
+            {
+                value[1]++;
+            }
+            else 
+            {
+                value[1] = 0;
+                if(value[0] < 5)
+                {
+                    value[0]++;
+                }
+                else
+                {
+                    value[0] = 0;
+                    value[1] = 0;
+                    value[2] = 0;
+                    value[3] = 0;
+                }
+            }
+        }
+    }
+}
