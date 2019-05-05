025520f2337773cd17f974a63b8d1882c4b34bf2
mz8023yt <mz8023yt@163.com>
2019-05-05 21:16:34 +0800

    [CLONE] 基于02.01开发02.02

---
+++ Demo/02.按键/02.按键控制数码管数值移位和加减/User/main.c
@@ -0,0 +1,27 @@
+#include "reg52.h"
+#include "main.h"
+#include "bsp_delay.h"
+#include "bsp_key.h"
+
+#define Segment P0
+#define Digital P2
+
+/* 数码管段选码、位选码 */
+u8 SegCode[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};      // 段选低电平有效
+u8 DigCode[4] = {0xfe, 0xfd, 0xfb, 0xf7};                                           // 位选低电平有效
+
+/* 数码管显示的值 */
+int value[4] = {0, 0, 0, 0};
+
+int main(void)
+{
+    u8 data1 = 0;
+
+    while(1)
+    {
+        Digital = DigCode[0];
+        Segment = SegCode[value[0]];
+        delay_ms(10);
+        key_control(key_scan());
+    }
+}
