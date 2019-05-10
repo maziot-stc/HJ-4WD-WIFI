6391dce8c3fa7e59f57a41c6ec68cc39d5e23dd4
mz8023yt <mz8023yt@163.com>
2019-05-11 01:00:17 +0800

    [FIX] STC单片机printf可以正确打印%x，但需要手动指定存储格式

--- Demo/04.串口/03.封装printf打印函数/User/main.c
+++ Demo/04.串口/03.封装printf打印函数/User/main.c
@@ -7,9 +7,8 @@ int main(void)
 {
     uart_init();
 
-    /* 验证下来发现 %x 无法正确打印，其他格式没有问题 */
-    /* 打印结果为: H 1000 demo 5500 Hello World! */
-    printf("%c %d %s %x Hello World!\r\n", 'H', (int)1000, "demo", (int)0x55);
+    // 使用C51的printf()函数打印%d/i/u/o/x/X格式时，必须要指定该变量的存储格式l/L/b/B
+    printf("%c %d %s %bx Hello World!\r\n", 'H', 1000, "demo", 0x55);
 
     while(1);
 }
